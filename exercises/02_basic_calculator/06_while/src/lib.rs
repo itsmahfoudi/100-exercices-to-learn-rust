7ff0355f1ba5879fc091dfe70205888b7419fa21
= <ayoubelmahfoudi05@gmail.com>
2024-05-18 17:18:27 +0100

    Finished the calculator section

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut counter:u32 = n;
+    let mut res:u32 = 1;
+    while counter > 0 {
+        res = res * counter;
+        counter = counter - 1;
+    }
+    res
 }
 
 #[cfg(test)]
